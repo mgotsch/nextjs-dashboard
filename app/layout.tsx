4e392b992db302a2346beb72e86c7cea2dbf7030
Mac Gotsch <mac@acadiantdigital.com>
2023-12-15 13:43:11 -0500

    Adding metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'Sample Dashboard from the Next.js Course, built with the App Router by Mac Gotsch.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
